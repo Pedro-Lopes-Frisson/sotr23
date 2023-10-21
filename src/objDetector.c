9eda77356919cb4fefe564e3350dca67aeb57052
Gonçalo Leal <goncalolealsilva@ua.pt>
2023-10-21 18:31:18 +0100

    project structure

---
+++ src/objDetector.c
@@ -0,0 +1,9 @@
+/******************************************************
+ * Gonçalo Leal - 98008
+ * goncalolealsilva@ua.pt
+ * 
+ * Pedro xxxx - xxxxx
+ * xxx@ua.pt
+ * 
+ * Responsible for all the image analysis tasks such as obstacles detection, object detection, etc.
+ */
